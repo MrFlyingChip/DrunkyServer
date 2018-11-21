41d2cd10d124de128192ad766f227181e8198f88
Kirill Minaev <kv_l@ukr.net>
2018-11-21 17:46:41 +0200

    filters admin support

---
+++ app/controllers/filterTypeController.js
@@ -0,0 +1,117 @@
+const filterType = require('../models/filterType');
+
+//filter type
+module.exports.fetchFilterType = function (db, req, res) {
+    const filterTypeId = req.params['id'];
+    filterType.fetchFilterType(db, filterTypeId)
+        .then(value => {
+            res.send(value);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+};
+
+module.exports.fetchFilterTypeProduct = function (db, req, res) {
+    const product = req.params['product'];
+    filterType.fetchFilterTypesProduct(db, product)
+        .then(value => {
+            res.send(value);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+};
+
+module.exports.createFilterType = function (db, req, res) {
+    const filterTypeObj = req.body;
+    filterType.createFilterType(db, filterTypeObj)
+        .then(value => {
+            res.send(value);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+};
+
+module.exports.updateFilterType = function (db, req, res) {
+    const filterTypeId = req.params['id'];
+    const filterTypeObj = req.body;
+    filterType.updateFilterType(db, filterTypeId, filterTypeObj)
+        .then(value => {
+            res.send(value);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+};
+
+module.exports.deleteFilterType = function (db, req, res) {
+    const filterTypeId = req.params['id'];
+    filterType.deleteFilterType(db, filterTypeId)
+        .then(value => {
+            res.send(value);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+};
+
+//filter
+module.exports.fetchFilter = function (db, req, res) {
+    const filterId = req.params['id'];
+    filterType.fetchFilter(db, filterId)
+        .then(value => {
+            res.send(value);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+};
+
+module.exports.fetchFiltersFilterType= function (db, req, res) {
+    const filterTypeId = req.params['id'];
+    filterType.fetchFiltersFilterType(db, filterTypeId)
+        .then(value => {
+            res.send(value);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+};
+
+module.exports.createFilter = function (db, req, res) {
+    const filterObj = req.body;
+    filterObj.image = req.file.originalname;
+    filterType.createFilter(db, filterObj)
+        .then(value => {
+            res.send(value);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+};
+
+module.exports.updateFilter = function (db, req, res) {
+    const filterId = req.params['id'];
+    const filterObj = req.body;
+    filterObj.image = (req.file) ? req.file.originalname : filterObj.image;
+    filterType.updateFilter(db, filterId, filterObj)
+        .then(value => {
+            res.send(value);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+};
+
+module.exports.deleteFilter = function (db, req, res) {
+    const filterId = req.params['id'];
+    filterType.deleteFilter(db, filterId)
+        .then(value => {
+            res.send(value);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+};
--- app/routes/admin_routes.js
+++ app/routes/admin_routes.js
@@ -1,6 +1,7 @@
 const product_controller = require('../controllers/product_controller');
 const ingredientController = require('../controllers/ingredientController');
 const toolController = require('../controllers/toolController');
+const filterTypeController = require('../controllers/filterTypeController');
 
 module.exports = function (app, db, upload) {
     //ingredient admin routes
@@ -43,5 +44,47 @@ module.exports = function (app, db, upload) {
 
     app.delete('/admin/tool/:id', (req, res) => {
         toolController.deleteTool(db, req, res);
+    });
+
+    //filterType admin routes
+    app.get('/admin/filterType/:id', (req, res) => {
+        filterTypeController.fetchFilterType(db, req, res);
+    });
+
+    app.get('/admin/filterType/:product', (req, res) => {
+        filterTypeController.fetchFilterTypeProduct(db, req, res);
+    });
+
+    app.post('/admin/filterType', upload.single('image'), (req, res) => {
+        filterTypeController.createFilterType(db, req, res);
+    });
+
+    app.put('/admin/filterType/:id', upload.single('image'), (req, res) => {
+        filterTypeController.updateFilterType(db, req, res);
+    });
+
+    app.delete('/admin/filterType/:id', (req, res) => {
+        filterTypeController.deleteFilterType(db, req, res);
+    });
+
+    //filter admin routes
+    app.get('/admin/filter/:id', (req, res) => {
+        filterTypeController.fetchFilter(db, req, res);
+    });
+
+    app.get('/admin/filters/:id', (req, res) => {
+        filterTypeController.fetchFiltersFilterType(db, req, res);
+    });
+
+    app.post('/admin/filter', upload.single('image'), (req, res) => {
+        filterTypeController.createFilter(db, req, res);
+    });
+
+    app.put('/admin/filter/:id', upload.single('image'), (req, res) => {
+        filterTypeController.updateFilter(db, req, res);
+    });
+
+    app.delete('/admin/filter/:id', (req, res) => {
+        filterTypeController.deleteFilter(db, req, res);
     });
 };
