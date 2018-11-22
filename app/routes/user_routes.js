9b84e1cafa0298a5610fef424a3a0f4c325740a5
Kirill Minaev <kv_l@ukr.net>
2018-11-22 18:24:30 +0200

    user routes added

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,3 +1,15 @@
-module.exports = function (app, db, upload) {
+const userController = require('../controllers/userController');
 
+module.exports = function (app, db, upload) {
+    app.post('/user/signUp', (req, res) => {
+        userController.signUp(req, res, db);
+    });
+
+    app.post('/user/logIn', (req, res) => {
+        userController.logIn(req, res, db);
+    });
+
+    app.post('/user/account', (req, res) => {
+        userController.accountInfo(req, res, db);
+    });
 };
